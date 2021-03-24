c6d8ba1c63db1bb6cb950a2295f9de0fcabaad29
talisson394b <talisson@raspberrypi>
2021-03-23 23:22:17 -0300

    Organize v2

--- organize.sh
+++ organize.sh
@@ -2,30 +2,44 @@
 
 MAIN_DIR="$HOME/Downloads"
 
-IMAGEM=("$HOME/Pictures" '.jpg' '.png' '.jpeg' '.gif')
-AUDIO=("$HOME/Music" '.mp3' '.m4a' '.wma')
-VIDEO=("$HOME/Pictures" '.mp4' '.mkv')
-DOCUMENTO=("$HOME/Documents" '.pdf')
 
-# Verifica se o diretório principal contem algum arquivo
-if [ $(ls -A $MAIN_DIR | wc -l) -eq 0 ]; then
+Property() {
+   echo -e "
+   "$HOME/Documents" "pdf" "html"\n
+   "$HOME/Music" "mp3" "m4a" "wma"\n
+   "$HOME/Movies" "mp4" "mkv"\n
+   "$HOME/Pictures" "jpg" "png" "jpeg" "gif"
+   "
+}
+
+
+Search() {
+    local extension=$1
+    grep "$extension" <<< $(Property)
+}
+
+
+Organize() {
+    local target_directory=$1 extension=$2
+    [ -d $target_directory ] || mkdir $target_directory
+    find $MAIN_DIR -name "*[.]$extension" -exec mv {} $target_directory \;
+}
+
+Main() {
+    local extension='' result=''
+    for file in "$MAIN_DIR"/*; do
+        extension="$(cut -f2 -d. <<< $file)
+        result="$(Search $extension)"
+
+	if [ -e $file ] && ! [ -z $extension ]; then
+	    declare -a CLASS=$result
+	    Organize ${CLASS[0]} $extension 
+	fi
+   done
+}
+
+if [ $( wc -l <<< $(ls -A $MAIN_DIR)) -eq 0 ]; then
     exit 0
 fi
 
-
-
-Main() {
-    declare -a array=($@)
-    local dir=${array[0]}
-
-    [ -d $dir ] || mkdir $dir
-    for ext in ${array[@]:1}; do
-        find $MAIN_DIR -name "*$ext" -exec mv {} $dir \;
-    done
-}
-
-Main ${IMAGEM[@]}
-Main ${AUDIO[@]}
-Main ${VIDEO[@]}
-Main ${DOCUMENTO[@]}
-
+Main
